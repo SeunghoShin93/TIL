40f0188336e48c188e66e0998398f7a3b5e42456
SeunghoShin93 <tmdghshin@gmail.com>
2019-09-16 17:58:04 +0900

    permutation

---
+++ Practice/untitled/Brute_Force 0916/Dongchul/Distribution.py
@@ -0,0 +1,23 @@
+from pprint import pprint
+import sys
+sys.stdin = open('input.txt', 'r')
+
+T = int(input())
+
+N = int(input())
+
+arr = [list(map(float, input().split())) for _ in range(N)]
+
+for n in range(N):
+    for i in range(N):
+        if n == i:
+            continue
+        for j in range(N):
+            if j == n or j == i:
+                continue
+            for k in range(N):
+                if k == n or k == i or k == j:
+                    continue
+
+                print((arr[n][0] * arr[i][1] *
+                       arr[j][2] * arr[k][3]) / 100**(N-1))
