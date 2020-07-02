fff7a1e0dec0fc2d32e1efd4dac792b11d19c2b4
SeunghoShin93 <tmdghshin@gmail.com>
2020-07-02 23:59:54 +0900

    ,

---
+++ 08_java/00_intro/src/str.java
@@ -0,0 +1,13 @@
+
+public class str {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		System.out.println("The length of the txt string is: " + txt.length());
+		String txt2 = "Hello World";
+		System.out.println(txt2.toUpperCase());   // Outputs "HELLO WORLD"
+		System.out.println(txt2.toLowerCase());   // Outputs "hello world"
+	}
+
+}
