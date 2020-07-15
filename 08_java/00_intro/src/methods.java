f69642a4360e7b4022b4c98aafb08bd882e9cb44
SeunghoShin93 <tmdghshin@gmail.com>
2020-07-15 23:00:35 +0900

    JAVA_methods

---
+++ 08_java/00_intro/src/methods.java
@@ -0,0 +1,18 @@
+
+public class methods {
+	static String add(String a, String b) {
+		return a + b;
+		
+	}
+	
+	static int add(int a, int b) {
+		return a + b;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(add("Hello ", "Java"));
+		System.out.println(add(3, 5));
+	}
+
+}
